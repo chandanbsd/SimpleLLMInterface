883e89876a66d1c42d5fbb0764aea0dc1ccc114c
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2025-02-10 01:10:37 -0500

    Integrate with lama model

--- SimpleLLMInterface.AppHost/Program.cs
+++ SimpleLLMInterface.AppHost/Program.cs
@@ -1,5 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var ollama = builder.AddOllama("ollama")
+    .WithDataVolume()
+    .WithOpenWebUI();
+
+var llama = ollama.AddModel("llama3");
+
 builder.AddProject<Projects.SimpleLLMInterface_Web>("Web");
 
 builder.Build().Run();
